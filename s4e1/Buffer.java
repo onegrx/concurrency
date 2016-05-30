0a6712fc070da25a6d372cdc0689a897bf0f04b6
Bartłomiej Szałach <onegrx@gmail.com>
2016-05-30 11:40:56 +0200

    Chain of producers and consumers with buffer.

---
+++ s4e1/Buffer.java
@@ -0,0 +1,50 @@
+package s4e1;
+
+import java.util.concurrent.locks.*;
+
+/**
+ * Created by onegrx on 05.04.16.
+ */
+public class Buffer {
+
+    private final Lock lock = new ReentrantLock();
+    private final int size = 20;
+
+    private Condition[] condition = new Condition[size];
+    private int[] available = new int[size];
+
+    public Buffer() {
+        for (int i = 0; i < size; i++) {
+            condition[i] = lock.newCondition();
+            available[i] = 0;
+        }
+    }
+
+    public void takeItem(int i) {
+        lock.lock();
+        try {
+            if(available[i] == 0) {
+                condition[i].await();
+            }
+            available[i]--;
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void returnItem(int i) {
+        lock.lock();
+        try {
+            available[i]++;
+            condition[i].signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
---
+++ s4e1/Consumer.java
@@ -0,0 +1,38 @@
+package s4e1;
+
+import java.util.*;
+
+/**
+ * Created by onegrx on 05.04.16.
+ */
+public class Consumer implements Runnable {
+
+    private Buffer monitor;
+    public int speed = 0;
+    public int number = 0;
+
+    private int bufferSize;
+    private final Random rand = new Random();
+
+    public Consumer(Buffer monitor, int speed, int number) {
+        this.monitor = monitor;
+        this.speed = speed;
+        this.number = number;
+        bufferSize = monitor.getSize();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            if(rand.nextInt(10) % speed == 0) {
+                monitor.takeItem(number % bufferSize);
+                number = (number + 1) % bufferSize;
+            }
+        }
+    }
+
+    public void process(int i) {
+        System.out.println("Processing cell: " + i);
+    }
+
+}
